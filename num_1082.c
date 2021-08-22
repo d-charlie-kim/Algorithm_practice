863db378a407ea17d887132d17b3b5b2a5baa7a4
charlie com <dokkim@student.42seoul.kr>
2021-08-23 00:25:24 +0900

    1082 greedy algorithm

--- num_1082.c
+++ num_1082.c
@@ -2,12 +2,49 @@
 
 int main()
 {
-	int size;
-	//A = 음수를 다 곱한 수
-	//B = 음수에서 -1을 빼고 다 곱한 수
-	//C = 
-	int A = 0, B = 0, C = 0, D = 0, E = 0, F = 0;
-	int nums[10001] = { 0, };
-	for (int i = 0; i < size; i++)
-		scanf("%d", nums[i]);
+	int N, M, goal = 0;
+	scanf ("%d", &N);
+	int nums[11] = { 0, };
+	for (int i = 0; i < N; i++)
+		scanf("%d", &(nums[i]));
+	scanf("%d", &M);
+	
+	// 최대 숫자의 자릿수 (size) 확인
+	int min = 51, size;
+	for (int i = 1; i < N; i++)
+	{
+		if (min > nums[i])
+			min = nums[i];
+	}
+	if (M < min)
+	{
+		printf("0\n");
+		return (0);
+	}
+	size = M / min;
+	// 가장 싼 숫자 많이 사는 것 과
+	// 가장 싼 숫자 하나와 숫자 '0' 많이 사는 것 비교
+	if (size < ((M - min) / nums[0]) + 1)
+		size = ((M - min) / nums[0]) + 1;
+	if (min > nums[0])
+		min = nums[0];
+
+	// 가장 큰 숫자 하나 사고 / 가장 싼 숫자 많이 샀을 때 자릿수 비교
+	// 최대 숫자의 자릿수와 같다면 해당하는 수 출력.
+	// 반복.
+	int count = 0, temp = 0;
+	for (int i = 1; size > 0 && i < N; i++)
+	{
+		temp = M - nums[N - i];
+		if (temp >= 0 && (temp / min) == size - 1)
+		{
+			printf("%d", N - i);
+			size--;
+			M = M - nums[N - i];
+			i--;
+		}
+	}
+	for (; size > 0; size--)
+		printf("0");
+	printf("\n");
 }
