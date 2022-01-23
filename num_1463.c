e2d3ec46d7f615d1d21e8fb65a46bfa8c253d722
charlie com <dokkim@student.42seoul.kr>
2022-01-23 22:45:14 +0900

    [add] 1463 : recursive

--- num_1463.c
+++ num_1463.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
+int ret = 2147483647;
+
+void recursive(int num, int count)
+{
+	if (count > ret)
+		return ;
+	if (num == 1)
+		ret = count;
+	if (num % 3 == 0)
+		recursive(num / 3, count + 1);
+	if (num % 2 == 0)
+		recursive(num / 2, count + 1);
+	recursive(num - 1, count + 1);
+}
+
 int main()
 {
 	int num;
-	int count = 0;
 
 	scanf("%d", &num);
-	if (num % 3)
-	{
-		
-	}
+	recursive(num, 0);
+	printf("%d\n", ret);
 }
