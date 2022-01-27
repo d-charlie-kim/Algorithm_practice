d0650e86e5b55d42829c8dbc9ea53c224b77a805
charlie com <dokkim@student.42seoul.kr>
2022-01-27 23:39:51 +0900

    [add] 11055 : recursive

--- num_11055.c
+++ num_11055.c
@@ -1,24 +1,24 @@
-//recursive different way
-
 #include <stdio.h>
 
 int	ret = 1;
 int N;
 int nums[1001];
-int	visit[1001] = { 0, };
+int	num_sum[1001] = { 0, };
 
 void	checking(int index)
 {
-	int big = 1;
+	int big = 0;
 
 	if (index < 0)
 		return ;
 	for (int i = index + 1; i < N; i++)
 	{
-		if (nums[index] > nums[i])
-			big = visit[i] + 1;
-		if (big > visit[index])
-			visit[index] = big;
+		if (nums[index] < nums[i])
+			big = num_sum[i] + nums[index];
+		else
+			big = nums[index];
+		if (big > num_sum[index])
+			num_sum[index] = big;
 	}
 	checking(index - 1);
 }
@@ -28,15 +28,17 @@ int main(void)
 	scanf ("%d", &N);
 	for (int i = 0; i < N; i++)
 		scanf("%d", &nums[i]);
-	visit[N - 1] = 1;
+	num_sum[N - 1] = nums[N - 1];
 	if (N > 1)
 	{
 		checking(N - 2);
 		for (int i = 0; i < N; i++)
 		{
-			if (ret < visit[i])
-				ret = visit[i];
+			if (ret < num_sum[i])
+				ret = num_sum[i];
 		}
 	}
+	else
+		ret = nums[0];
 	printf("%d\n", ret);
 }
