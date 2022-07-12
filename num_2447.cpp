92f61a4aa0f3518d35c78a83975dc6494aaa2d6e
dokkim <dokkim@student.42seoul.kr>
2022-07-12 20:06:14 +0900

    [2447] : recursive

--- num_2447.cpp
+++ num_2447.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 
-void recursive(int x, int y, int z, char c, int size)
+// void recursive(int x, int y, int z, char c, int size)
+// {
+// 	if (z == 1)
+// 	{
+// 		std::cout << c;
+// 		if (x == 2)
+// 			std::cout << std::endl;
+// 	}
+// 	else
+// 	{
+// 		recursive(0, 0, z / 3,  c, size);
+// 		recursive(1, 0, z / 3,  c, size);
+// 		recursive(2, 0, z / 3,  c, size);
+// 		recursive(0, 1, z / 3,  c, size);
+// 		recursive(1, 1, z / 3, ' ', size);
+// 		recursive(2, 1, z / 3,  c, size);
+// 		recursive(0, 2, z / 3,  c, size);
+// 		recursive(1, 2, z / 3,  c, size);
+// 		recursive(2, 2, z / 3,  c, size);
+// 	}
+// }
+
+void recursive(int x, int y, int z)
 {
-	if (z == 1)
-	{
-		std::cout << c;
-		if (x == 2)
-			std::cout << std::endl;
-	}
+	if ((x / z) % 3 == 1 && (y / z) % 3 == 1)
+		std::cout << " ";
+	else if (z == 1)
+		std::cout << "*";
 	else
-	{
-		recursive(0, 0, z / 3,  c, size);
-		recursive(1, 0, z / 3,  c, size);
-		recursive(2, 0, z / 3,  c, size);
-		recursive(0, 1, z / 3,  c, size);
-		recursive(1, 1, z / 3, ' ', size);
-		recursive(2, 1, z / 3,  c, size);
-		recursive(0, 2, z / 3,  c, size);
-		recursive(1, 2, z / 3,  c, size);
-		recursive(2, 2, z / 3,  c, size);
-	}
+		recursive(x, y, z / 3);
 }
 
 int main()
 {
 	int size = 0;
 	std::cin >> size;
-	recursive(0, 0, size, '*', size);
+	for (int x = 0; x < size; x++)
+	{
+		for (int y = 0; y < size; y++)
+			recursive(x, y, size);
+		std::cout << std::endl;
+	}
 	return 0;
 }
-
