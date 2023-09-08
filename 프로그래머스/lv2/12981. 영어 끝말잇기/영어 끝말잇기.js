88c909b9dc2ee0043dc74cdc667610fd08b3dd13
Charlie Kim <d.charlie.kim@gmail.com>
2023-09-08 21:40:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,14 @@
+function solution(n, words) {
+    const answer = [0,0];
+    
+    for (let i = 1; i < words.length; i++) {
+        if (words.indexOf(words[i]) !== i 
+            || (words[i - 1].split('').reverse().join()[0] != words[i][0])) {
+            answer[0] = (i + 1) % n === 0 ? n : (i + 1) % n;
+            answer[1] = Math.ceil((i + 1) / n);
+            break;
+}
+}
+
+    return answer;
+}
