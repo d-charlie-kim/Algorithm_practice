a0fc85565550a3320ef6ea34ad55cf4cecbab19d
Charlie Kim <d.charlie.kim@gmail.com>
2023-09-11 17:05:03 +0900

    [level 1] Title: 시저 암호, Time: 3.02 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,14 +1,22 @@
 function solution(s, n) {
-  const smallAlpha = "abcdefghijklmnopqrstuvwxyz";
-  const bigAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	let answer = "";
+    var answer = '';
+    let sArr = s.split('');
 
-  for (const el of s) {
-    if (el === " ") answer += el;
-    if (smallAlpha.indexOf(el) !== -1)
-      answer += smallAlpha[(smallAlpha.indexOf(el) + n) % 26];
-    if (bigAlpha.indexOf(el) !== -1)
-      answer += bigAlpha[(bigAlpha.indexOf(el) + n) % 26];
-  }
-  return answer;
+    const upperArr = Array.from(
+        { length: 26 }, (_, index) => String.fromCharCode(65 + index)
+    );
+    const lowerArr = Array.from({ length: 26 }, (_, index) => String.fromCharCode(97 + index));
+    
+
+    sArr.map((el) => {
+        if (el === " ") {
+            answer += " ";
+          } else if (el === el.toUpperCase()) {
+                upperArr.indexOf(el) + n > 25? (answer += upperArr[(upperArr.indexOf(el) + n) - upperArr.length]) : (answer += upperArr[upperArr.indexOf(el) + n])
+          } else {
+                lowerArr.indexOf(el) + n > 25? answer += lowerArr[lowerArr.indexOf(el) + n - lowerArr.length] : answer += lowerArr[lowerArr.indexOf(el) + n] 
+
+          }
+    })
+    return answer;
 }
