3531eaa584481559cd53ebeea7727740445adc79
Charlie Kim <d.charlie.kim@gmail.com>
2023-09-21 23:44:27 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 4.94 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,15 +1,22 @@
-function solution(str) {
-  const history = {};
-  const answer = [];
+// function solution(str) {
+//   const history = {};
+//   const answer = [];
 
-  str.split("").forEach((alpha, idx) => {
-    if (Object.keys(history).includes(alpha)) {
-      answer.push(idx - history[alpha]);
-      history[alpha] = idx;
-    } else {
-      answer.push(-1);
-      history[alpha] = idx;
-    }
-  });
-  return answer;
+//   str.split("").forEach((alpha, idx) => {
+//     if (Object.keys(history).includes(alpha)) {
+//       answer.push(idx - history[alpha]);
+//       history[alpha] = idx;
+//     } else {
+//       answer.push(-1);
+//       history[alpha] = idx;
+//     }
+//   });
+//   return answer;
+// }
+
+function solution(str) {
+    const answer = [];
+    for (let i = 0; i < str.length; i++)
+        answer.push(str.indexOf(str[i]) === i ? -1 : i - str.lastIndexOf(str[i], i - 1));
+    return answer;
 }
