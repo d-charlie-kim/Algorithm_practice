3b0e61615204a9b641a8d96cb3766c20dbf1243c
Charlie Kim <d.charlie.kim@gmail.com>
2023-10-07 00:29:02 +0900

    [level 1] Title: 2016년, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,10 +1,10 @@
 function solution(a, b) {
   const days = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
-  const months = [30, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   let count = 0;
 
   for (let i = a - 1; i >= 1; i--) count += months[i - 1];
-  count += count === 0 ? b - 1 : b;
+  count += b;
 
-  return days[parseInt(count % 7)];
+  return days[parseInt((count - 1) % 7)];
 }
