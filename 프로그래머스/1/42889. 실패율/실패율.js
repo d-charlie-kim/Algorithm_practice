766e33f0797f1d3a94984a5d233523b7c803496c
Charlie Kim <d.charlie.kim@gmail.com>
2023-11-07 21:19:04 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,31 +1,22 @@
 function solution(N, stages) {
-	stages.sort((a, b) => (a - b));
+  const data = [],
+    totalPlayers = stages.length;
 
-	let stage = 1; count = 0, total = stages.length, i = 0;
-	let fail = [], answer = [];
+  stages.sort((a, b) => a - b);
 
-	while (stage <= N) {
-		if (stage == stages[i]) {
-			count += 1;
-			i += 1;
-			continue;
-		}
-		else {
-			fail.push([stage, (count / total)]);
-			total -= count;
-			count = 0;
-			stage += 1;
-		}
-	}
+  for (let stage = 1; stage <= N; stage++) {
+    const stagePlayers = totalPlayers - stages.indexOf(stage);
+    const notClearPlayers =
+      stages.indexOf(stage) === -1
+        ? 0
+        : stages.lastIndexOf(stage) - stages.indexOf(stage) + 1;
 
-	fail.sort((a, b) => {
-			if (a[1] == b[1]) {
-				return (a[0] - b[0]);
-			}
-			return (b[1] - a[1]);
-		});
-	
-	for (let x of fail)
-			answer.push(x[0]);
-	return answer;
+    data.push([stage, notClearPlayers / stagePlayers]);
+  }
+
+  data.sort((a, b) => {
+    return b[1] - a[1];
+  });
+  const answer = data.map((record) => record[0]);
+  return answer;
 }
