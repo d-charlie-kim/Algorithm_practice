c172a7023709cec8c9bda739d3ee5f8aee2284ea
Charlie Kim <d.charlie.kim@gmail.com>
2024-04-24 01:18:36 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 128 ms, Memory: 9488 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.js
@@ -1,13 +1,15 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let N = parseInt(input[0]); // 파일의 개수
-let pattern = input[1].split("*");
-let front = pattern[0], back = pattern[1];
-
-for (let i = 2; i < 2 + N; i++) { // 파일 이름을 순회
+const N = parseInt(input[0]);
+let [front, back] = input[1].split("*");
+for (let i = 2; i < N + 2; i++) {
   let fileName = input[i];
-  if (fileName.startsWith(front) && fileName.endsWith(back) && fileName.length >= front.length + back.length) {
+  if (
+    fileName.startsWith(front) &&
+    fileName.endsWith(back) &&
+    fileName.length >= front.length + back.length
+  ) {
     console.log("DA");
   } else {
     console.log("NE");
