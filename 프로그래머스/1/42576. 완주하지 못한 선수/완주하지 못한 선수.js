bd85c341dba0815b50663b7c713d3e2d2206a354
Charlie Kim <d.charlie.kim@gmail.com>
2024-07-09 16:29:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.63 ms, Memory: 66.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,31 @@
-function solution(participant, completion) {
-  participant.sort();
-  completion.sort();
+// function solution(participant, completion) {
+//   participant.sort();
+//   completion.sort();
 
-  for (let i = 0; i < completion.length; i++) {
-    if (participant[i] != completion[i]) return participant[i];
-  }
-  return participant.pop();
+//   for (let i = 0; i < completion.length; i++) {
+//     if (participant[i] != completion[i]) return participant[i];
+//   }
+//   return participant.pop();
+// }
+
+function solution(participant, completion) {
+    const map = new Map();
+
+    for (const name of participant) {
+        if (!map.has(name))
+            map.set(name, 1);
+        else {
+            map.set(name, map.get(name) + 1);
+        }
+    }
+
+    for (const name of completion) {
+        map.set(name, map.get(name) - 1);
+    }
+
+    for (const [key, value] of map.entries()) {
+        if (value === 1) {
+            return (key);
+        }
+    }
 }
