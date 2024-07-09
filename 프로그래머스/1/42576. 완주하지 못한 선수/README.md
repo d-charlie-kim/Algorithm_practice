ca3a3a549598e872ecb4ced82623393424af7e52
Charlie Kim <d.charlie.kim@gmail.com>
2024-07-09 16:04:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.47 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 56.9 MB, 시간: 92.52 ms
+메모리: 56.6 MB, 시간: 91.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 19:56:22
+2024년 07월 09일 16:04:32
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,9 +1,9 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
+  participant.sort();
+  completion.sort();
 
-    for (let i = 0; i < participant.length; i++) {
-        if (participant[i] != completion[i])
-            return participant[i];
-    }
+  for (let i = 0; i < completion.length; i++) {
+    if (participant[i] != completion[i]) return participant[i];
+  }
+  return participant.pop();
 }
