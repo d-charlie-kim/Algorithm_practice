484291f42eb51a0ff54e348c58e30fd98e93a618
Charlie Kim <d.charlie.kim@gmail.com>
2024-08-13 15:40:41 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 114.03 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,67 +1,41 @@
-function bfs(x, y, land, areaIdx) {
-  const directions = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0],
-  ];
-  const queue = [[x, y]];
-  let count = 1;
-  land[x][y] = areaIdx;
-
-  while (queue.length) {
-    const [nowX, nowY] = queue.shift();
-    directions.forEach((direction) => {
-      const newX = direction[0] + nowX;
-      const newY = direction[1] + nowY;
-
-      if (
-        newX >= 0 &&
-        newX < land.length &&
-        newY >= 0 &&
-        newY < land[0].length &&
-        land[newX][newY] === 1
-      ) {
-        count += 1;
-        queue.push([newX, newY]);
-        land[newX][newY] = areaIdx;
-      }
-    });
-  }
-  return count;
-}
-
 function solution(land) {
-  const rowList = new Map();
-  const areaSizes = new Map();
-
-  let areaIdx = 2;
-  for (let y = 0; y < land[0].length; y++) {
-    for (let x = 0; x < land.length; x++) {
-      if (!rowList.has(y)) {
-        rowList.set(y, []);
-      }
-      if (
-        land[x][y] !== 1 &&
-        land[x][y] !== 0 &&
-        !rowList.get(y).includes(land[x][y])
-      ) {
-        rowList.get(y).push(land[x][y]);
+  const dfs = (i, j, positions) => {
+    const stack = [[i, j]];
+    let cnt = 0;
+    
+    while (stack.length) {
+      const [x, y] = stack.pop();
+      
+      if (x < 0 || x >= land.length || y < 0 || y >= land[x].length || land[x][y] !== 1) {
         continue;
       }
-
-      if (land[x][y] === 1) {
-        areaSizes.set(areaIdx, bfs(x, y, land, areaIdx));
-        rowList.get(y).push(areaIdx);
-        areaIdx += 1;
-      }
+      
+      land[x][y] = 0;
+      cnt += 1;
+      positions.add(y);
+      
+      stack.push([x + 1, y]);
+      stack.push([x - 1, y]);
+      stack.push([x, y + 1]);
+      stack.push([x, y - 1]);
     }
-  }
+    
+    return cnt;
+  };
 
-  let maxCount = 0;
-  for (const areas of rowList.values()) {
-    const count = areas.reduce((ac, cu) => ac + areaSizes.get(cu), 0);
-    maxCount = Math.max(maxCount, count);
-  }
-  return maxCount;
+  const cnts = new Map();
+  land.forEach((row, i) => {
+    row.forEach((item, j) => {
+      if (item === 1) {
+        const positions = new Set();
+        const cnt = dfs(i, j, positions);
+        
+        for (let j of positions) {
+          cnts.set(j, (cnts.get(j) || 0) + cnt);
+        }
+      } 
+    });
+  });
+
+  return Math.max(...cnts.values()) || 0;
 }
